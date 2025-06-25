4ad766b56360da03a0683382cd32e43e958f4ad1
JimmyAlbarrac1n <jimmismael2003@gmail.com>
2025-06-25 00:23:29 -0500

    Cambios para subir datos quemados de sqlite a render

--- app.py
+++ app.py
@@ -14,9 +14,19 @@ migrate = Migrate(app, db)
 
 print("Ruta absoluta de la base de datos:", os.path.abspath('db.sqlite3'))
 
-# Ahora importa y registra blueprints
 from controllers.venta_controller import main_blueprint
 app.register_blueprint(main_blueprint)
+
+def init_db():
+    db_path = os.path.join(os.path.dirname(__file__), 'db.sqlite3')
+    if not os.path.exists(db_path):
+        with sqlite3.connect(db_path) as conn:
+            with open(os.path.join(os.path.dirname(__file__), 'data.sql'), encoding='utf-8') as f:
+                conn.executescript(f.read())
+
+# Llama a esta función ANTES de registrar blueprints
+with app.app_context():
+    init_db()
 
 if __name__ == "__main__":
     app.run(debug=True)
